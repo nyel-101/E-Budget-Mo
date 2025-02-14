3d8f37caf20aed26ec499a7cef46695ce3da6be8
nyel-101 <dandevjr@gmail.com>
2025-02-14 21:08:49 +0000

    style(core): fix database index

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2314ca96425b4bd28a4cc6b437eca61d = () => {
-  return '2026-01-30T17:03:16Z';
+const node_0d89a80365c8437fb993f0a96d0562b6 = () => {
+  return '2025-02-14T21:08:49Z';
 };
