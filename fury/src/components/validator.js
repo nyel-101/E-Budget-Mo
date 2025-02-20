f8c96330ec756c94c3cb2c56aa706096ce6e83c3
nyel-101 <dandevjr@gmail.com>
2025-02-20 09:11:13 +0000

    fix(ui): optimize payload parser

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_803f9e22ed8a4238b1df92e0416b7a4d = () => {
-  return '2026-01-27T07:34:17Z';
+const node_e385a5c1aa5b4c3b90c8d296a0dc3872 = () => {
+  return '2025-02-20T09:11:13Z';
 };
