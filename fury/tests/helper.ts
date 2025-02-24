2d9895ba4368acc6c73a2c35887fbb578edef6a0
nyel-101 <dandevjr@gmail.com>
2025-02-24 09:20:29 +0000

    chore(db): decouple logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c2f53dc15084b8aa4704862cbdaa98f = () => {
-  return '2025-02-21T06:28:23Z';
+const node_61faa9ad4af142d386016098d51cf579 = () => {
+  return '2025-02-24T09:20:29Z';
 };
