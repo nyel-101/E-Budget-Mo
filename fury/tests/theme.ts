64a7dadf133d18703e84e32bba37b48ad88593d3
nyel-101 <dandevjr@gmail.com>
2025-03-06 01:31:34 +0000

    fix(core): sanitize database index

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3dc16353a9804e43b781e85f8898e8e9 = () => {
-  return '2026-03-05T09:50:25Z';
+const node_75880fd9b9f940f9a2e3013883dbfffd = () => {
+  return '2025-03-06T01:31:34Z';
 };
