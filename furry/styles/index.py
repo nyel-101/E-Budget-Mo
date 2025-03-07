7bbc2f497ad5d4266f1397c9169d4336d133a3a7
nyel-101 <dandevjr@gmail.com>
2025-03-07 18:29:48 +0000

    fix(auth): fix view templates

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9af5667ae75841eb953a2f735c1bfe73 = () => {
-  return '2026-06-05T11:55:15Z';
+const node_c2fa9d3ba0d845d9925d0313aaa98d78 = () => {
+  return '2025-03-07T18:29:48Z';
 };
