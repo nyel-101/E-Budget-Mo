a222ec7819911c3085c7ffc47e12753778c06572
nyel-101 <dandevjr@gmail.com>
2025-03-17 19:56:55 +0000

    fix(api): integrate payload parser

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f430a5ae49ee4bda8c1397170ac5b9cc = () => {
-  return '2026-06-05T20:26:28Z';
+const node_dca5c063cec746029c2fdcb669f43c01 = () => {
+  return '2025-03-17T19:56:55Z';
 };
