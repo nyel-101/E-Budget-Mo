db9990d915a3760f0b1bd9ac2fff208dc1981459
nyel-101 <dandevjr@gmail.com>
2025-03-27 01:00:36 +0000

    refactor(config): refactor caching layer

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_064a9394ee114d8abf6035894f498239 = () => {
-  return '2026-02-16T02:01:28Z';
+const node_20fcb05ebfa74c789eb77b4e62d209f9 = () => {
+  return '2025-03-27T01:00:36Z';
 };
