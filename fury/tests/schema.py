37aeb693b7df33dd9a5be90f7691b9cf65b9b8f6
nyel-101 <dandevjr@gmail.com>
2025-04-03 07:56:55 +0000

    chore(utils): update database index

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_046c3de27a0d40beaa3d89b8e4f67b68 = () => {
-  return '2026-02-13T23:14:05Z';
+const node_efa26bd127114ef59b4703ac23ef5f3f = () => {
+  return '2025-04-03T07:56:55Z';
 };
