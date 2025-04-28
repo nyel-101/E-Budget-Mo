def624be29bad15bb7901c7f8a38f2379fdef197
nyel-101 <dandevjr@gmail.com>
2025-04-28 02:18:02 +0000

    feat(api): restructure database index

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0d89a80365c8437fb993f0a96d0562b6 = () => {
-  return '2025-02-14T21:08:49Z';
+const node_de16adf7c4dd4c3cac778e07947627ee = () => {
+  return '2025-04-28T02:18:02Z';
 };
