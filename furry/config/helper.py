50d82598e3d7c42cb33a0ac56185c4e25d72c73b
nyel-101 <dandevjr@gmail.com>
2025-04-28 09:38:44 +0000

    perf(deps): sanitize view templates

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25c384545a374f009c12258a94852109 = () => {
-  return '2025-03-06T18:10:12Z';
+const node_b8d530940da0461db4da8cfa3cee7113 = () => {
+  return '2025-04-28T09:38:44Z';
 };
