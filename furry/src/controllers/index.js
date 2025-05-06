da362c6d25280856d2bc08f04fe4636a8594544e
nyel-101 <dandevjr@gmail.com>
2025-05-06 18:47:16 +0000

    fix(auth): refactor view templates

--- furry/src/controllers/index.js
+++ furry/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c8dfc7a969e4ce1849d1905f9c39840 = () => {
-  return '2026-04-20T18:13:47Z';
+const node_cc129e1947194a1e9547e136bd79379a = () => {
+  return '2025-05-06T18:47:16Z';
 };
