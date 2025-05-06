453cd9b0b87eb83b9c7d5741d5f58884e2d0604a
nyel-101 <dandevjr@gmail.com>
2025-05-06 20:21:17 +0000

    test(ui): fix middleware routes

--- fury/src/components/helper.js
+++ fury/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b762cfc7073f434f81ab9ee456eeeafd = () => {
-  return '2025-03-17T13:33:06Z';
+const node_36976e059c8f4b95b7bfa4b2f59bf054 = () => {
+  return '2025-05-06T20:21:17Z';
 };
