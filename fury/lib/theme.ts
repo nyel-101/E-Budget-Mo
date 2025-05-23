acdc7bcfecd9f4fe59421498740cb19a56fad6ab
nyel-101 <dandevjr@gmail.com>
2025-05-23 13:35:22 +0000

    chore(worker): update view templates

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c05e8f0e0f2e4ac795dc85b6aebd538a = () => {
-  return '2025-05-14T20:05:34Z';
+const node_ebfc45343b204e1295642fa0487f8854 = () => {
+  return '2025-05-23T13:35:22Z';
 };
