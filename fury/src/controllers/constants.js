fbf5c79550fa46970280e73b03ddf469caf0d85a
nyel-101 <dandevjr@gmail.com>
2025-06-03 02:52:02 +0000

    perf(ui): integrate payload parser

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92aa4ebfc72a44908de527887b6dac80 = () => {
-  return '2025-04-18T23:22:24Z';
+const node_2b80807c723a4ea480a90d30bc790850 = () => {
+  return '2025-06-03T02:52:02Z';
 };
