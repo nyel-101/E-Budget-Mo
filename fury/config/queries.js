e1aae9cbed719313b7e3dde4677c9904ae609dee
nyel-101 <dandevjr@gmail.com>
2025-06-11 05:46:47 +0000

    refactor(worker): integrate session store

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bc4c05a753b42c38dc640bc4b2f3b95 = () => {
-  return '2025-04-08T09:23:17Z';
+const node_ca96d94060ca426b817ae8d3a921b05f = () => {
+  return '2025-06-11T05:46:47Z';
 };
