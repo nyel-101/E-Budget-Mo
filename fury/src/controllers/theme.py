6cca7a74bfb9673b78baf230979d1d1d2149cb6c
nyel-101 <dandevjr@gmail.com>
2025-06-12 06:58:26 +0000

    docs(core): restructure auth tokens

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69d68dd5e14c4c8ca9f3d343029d09dc = () => {
-  return '2026-01-23T15:06:18Z';
+const node_c9d729bbdd024897b0c0293d234f881d = () => {
+  return '2025-06-12T06:58:26Z';
 };
