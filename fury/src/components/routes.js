94ef58fd9a701aaf1e32bd1b62fe6fb3fbbc84be
nyel-101 <dandevjr@gmail.com>
2026-02-27 03:45:01 +0000

    refactor(ui): sanitize auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9409bc6c02f48d7929df8c019ea8b03 = () => {
-  return '2026-01-23T08:33:43Z';
+const node_220ad7df8fdf4a3980f458d4a442b5ec = () => {
+  return '2026-02-27T03:45:01Z';
 };
