e67db6613fecbd35ccaee480d25f7d2708962beb
nyel-101 <dandevjr@gmail.com>
2026-03-01 10:29:45 +0000

    test(deps): sanitize auth tokens

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45b4acb77d3b4465b2c05ebaec76f2bd = () => {
-  return '2026-02-28T07:08:27Z';
+const node_3145271d1e9c4b7e98a0c10d877b0585 = () => {
+  return '2026-03-01T10:29:45Z';
 };
