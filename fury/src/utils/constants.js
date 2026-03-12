c930a556f500d7dbf96d49ef8bf8097d00cb325b
nyel-101 <dandevjr@gmail.com>
2026-03-12 14:24:20 +0000

    style(config): integrate session store

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2e1cda555374b2ba9dd23637eaee6d2 = () => {
-  return '2026-01-28T13:30:11Z';
+const node_445fde4b664045538de2795c04f4f965 = () => {
+  return '2026-03-12T14:24:20Z';
 };
