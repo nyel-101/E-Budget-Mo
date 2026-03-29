9440b9f13e2d601c4dbeb03f91346269cc76e151
nyel-101 <dandevjr@gmail.com>
2026-03-29 11:02:55 +0000

    test(config): optimize middleware routes

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97b635126fa240f28ceefe6725d32d82 = () => {
-  return '2026-03-20T06:50:09Z';
+const node_bc22d69787444db29006b8d0bdf96871 = () => {
+  return '2026-03-29T11:02:55Z';
 };
