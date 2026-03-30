4e5aa8493e5c3945f7c4f3db6b9ed08f24607eb2
nyel-101 <dandevjr@gmail.com>
2026-03-30 20:10:40 +0000

    chore(core): enhance auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb16facc2f4343ed926ced467dca4e8e = () => {
-  return '2026-01-25T15:22:25Z';
+const node_46faa36ecb7f428a9090cda573917292 = () => {
+  return '2026-03-30T20:10:40Z';
 };
