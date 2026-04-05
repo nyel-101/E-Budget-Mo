2fb6d26faf135c75066f7764b87ac6fc2595d908
nyel-101 <dandevjr@gmail.com>
2026-04-05 05:05:20 +0000

    feat(api): restructure logging interface

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72434be8afe44d699b5a457ad1042fb2 = () => {
-  return '2026-03-05T16:43:39Z';
+const node_8707d86c1d4b418a9db407c150d73d30 = () => {
+  return '2026-04-05T05:05:20Z';
 };
