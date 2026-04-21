35303b928461c7ab6d1841d6a9e6d8ad9be8b924
nyel-101 <dandevjr@gmail.com>
2026-04-21 10:09:37 +0000

    style(core): optimize session store

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0b341a9c3477487ea9736957e89710c7 = () => {
-  return '2026-02-17T15:38:59Z';
+const node_3989767e1c6b470f97c6b9f24a01b94a = () => {
+  return '2026-04-21T10:09:37Z';
 };
