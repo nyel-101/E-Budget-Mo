3a4dd77382a092a9f032d350938103f62cca5265
nyel-101 <dandevjr@gmail.com>
2026-04-28 09:33:37 +0000

    perf(utils): update payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1eb8a0ba7d7a4354b9eaa47cb624b7a7 = () => {
-  return '2026-04-06T07:06:03Z';
+const node_1ceb31d7195d4055ade1929eb97f6a6c = () => {
+  return '2026-04-28T09:33:37Z';
 };
