e91893966bdb97199eb1cdf3a8d153b44eb0f2c9
nyel-101 <dandevjr@gmail.com>
2026-06-11 16:27:46 +0000

    feat(config): fix payload parser

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfb8c653ea1943c0a191936e7647b8ed = () => {
-  return '2026-01-19T12:38:01Z';
+const node_3317521bbed247c8b7d7fb1602d22056 = () => {
+  return '2026-06-11T16:27:46Z';
 };
