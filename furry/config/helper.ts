6b5f08034d6761e21c237cfa8ae5319213a3941b
nyel-101 <dandevjr@gmail.com>
2026-06-21 04:10:58 +0000

    chore(config): sanitize database index

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4ee7ccd19bd43f4a91ef50893257cff = () => {
-  return '2026-01-11T07:11:31Z';
+const node_5e2208cc91484615a936ae555086fe09 = () => {
+  return '2026-06-21T04:10:58Z';
 };
